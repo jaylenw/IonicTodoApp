9bf7cf7b128dcdb6314585677a084e1de0ad58c8
jaylenw <mr2cleanj@live.com>
2016-07-27 08:35:34 +0000

    added factories

---
+++ www/js/services/user.js
@@ -0,0 +1,17 @@
+angular.module('starter')
+.factory("User",['$resource', function($resource){
+  return $resource("http://yourdomain.com:3000/users", {},{
+    login:{
+      method:"POST",
+      params:{},
+      isArray: false,
+      url: "http://yourdomain.com:3000/users/login"
+    },
+    register:{
+      method:"POST",
+      params:{},
+      isArray: false,
+      url: "http://yourdomain.com:3000/users/register"
+    }
+  })
+}]);
